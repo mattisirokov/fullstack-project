dcd49ce23e65354f55bdcbe99ea95bbafa48f977
Matti Sirokov <mattisirokov@gmail.com>
2022-09-24 19:39:18 +0300

    User route, controller and service created

---
+++ api/src/controllers/user.controller.ts
@@ -0,0 +1,106 @@
+import { Request, Response, NextFunction } from 'express'
+
+import User from '../models/User'
+import { BadRequestError } from '../helpers/apiError'
+import userService from '../services/user.service'
+
+// POST /users
+export const createUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { firstname, surname, username, email, password, isBanned } = req.body
+
+    const user = new User({
+      firstname,
+      surname,
+      username,
+      email,
+      password,
+      isBanned,
+    })
+
+    await userService.create(user)
+    res.json(user)
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', 400, error))
+    } else {
+      next(error)
+    }
+  }
+}
+
+// PUT /users/:userId
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const update = req.body
+    const userId = req.params.userId
+    const updatedUser = await userService.update(userId, update)
+    res.json(updatedUser)
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', 400, error))
+    } else {
+      next(error)
+    }
+  }
+}
+
+// DELETE /users/:userId
+export const deleteUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await userService.deleteUser(req.params.userId)
+    res.status(204).end()
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', 400, error))
+    } else {
+      next(error)
+    }
+  }
+}
+
+// GET /user/:userId
+export const findById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await userService.findById(req.params.userId))
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', 400, error))
+    } else {
+      next(error)
+    }
+  }
+}
+
+// GET /users
+export const findAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await userService.findAll())
+  } catch (error) {
+    if (error instanceof Error && error.name == 'ValidationError') {
+      next(new BadRequestError('Invalid Request', 400, error))
+    } else {
+      next(error)
+    }
+  }
+}
---
+++ api/src/models/User.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document } from 'mongoose'
+
+export type UserDocument = Document & {
+  firstname: string
+  surname: string
+  username: string
+  email: string
+  password: string
+  isBanned: boolean
+}
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isBanned: {
+    type: Boolean,
+    required: true,
+  },
+})
+export default mongoose.model<UserDocument>('User', userSchema)
+
+/*
+example:
+{
+    "firstname":"tester",
+    "surname":"tester surname",
+    "username": "testerusername",
+    "email": "tester@gmail.com"
+    "password": "notmyrealpassword",
+    "isBanned": false,
+}
+
+*/
